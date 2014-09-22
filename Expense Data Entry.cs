da6c9b2033eb764cc521625ad080da64295dd475
jaro003 <mjaro91@live.com>
2014-09-22 15:40:40 -0700

    Project Forms from Augustin

---
+++ Expense Data Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Forms
+{
+    public partial class Expense_Data_Entry : Form
+    {
+        public Expense_Data_Entry()
+        {
+            InitializeComponent();
+        }
+
+        private void Expense_Data_Entry_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
